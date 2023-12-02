3f30ddf2c0a2c8b0e7de561803285ffcb7ac16f7
Paul D'Ambra <paul@posthog.com>
2023-12-02 16:14:50 +0000

    part 2 day 2

--- src/day_two.rs
+++ src/day_two.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Error};
 
 fn is_over_max(set: &[&str], maximums: HashMap<&str, i32>) -> bool {
         println!("is over max: {:?}", set);
@@ -12,19 +12,7 @@ fn is_over_max(set: &[&str], maximums: HashMap<&str, i32>) -> bool {
 }
 
 pub(crate) fn day_two_part_one() -> Result<(), std::io::Error> {
-    println!("day two prart one");
-
-    let current_dir = env::current_dir()?;
-    let path = "src/day_two_puzzle_input.txt";
-    let absolute_path = current_dir.join(path);
-    println!("{:?}", absolute_path);
-    let file = File::open(absolute_path)?;
-
-    // Create a BufReader to efficiently read the file line by line
-    let reader = BufReader::new(file);
-    let max_red = 12;
-    let max_green = 13;
-    let max_blue = 14;
+    let reader = get_puzzle_input()?;
 
     let mut max_color = HashMap::new();
 
@@ -57,4 +45,51 @@ pub(crate) fn day_two_part_one() -> Result<(), std::io::Error> {
     println!("{}", valid_ids_sum);
 
     Ok(())
+}
+
+pub(crate) fn day_two_part_two() -> Result<(), std::io::Error> {
+    let reader = get_puzzle_input()?;
+
+    let mut powers: Vec<i32> = vec![];
+
+    for line_result in reader.lines() {
+        let mut line = line_result?;
+        println!("{}", line);
+        let mut first_parts: Vec<_> = line.split(':').collect();
+        let id = first_parts.first().unwrap().split(" ").nth(1).unwrap().parse::<i32>().unwrap();
+        println!("Game {}", id);
+        let sets: Vec<_> = first_parts.last().unwrap().trim().split(';').flat_map(|x|x.split(", ").map(str::trim)).map(|x|x.split(" ").collect::<Vec<_>>()).collect();
+        let mut max_color = HashMap::new();
+
+        max_color.insert("red", -1);
+        max_color.insert("green", -1);
+        max_color.insert("blue", -1);
+
+        for set in sets {
+            let color = set.last().unwrap();
+            let number = set.first().unwrap().parse::<i32>().unwrap();
+            let current_maximum = max_color.get(color).unwrap().clone();
+            if number > current_maximum {
+                max_color.insert(color, number);
+            }
+        }
+
+        let power = max_color.values().fold(1, |acc, &value| acc * value);
+        powers.push(power)
+    }
+    println!("sum of powers: {}", powers.iter().sum::<i32>());
+
+    Ok(())
+}
+
+fn get_puzzle_input() -> Result<BufReader<File>, Error> {
+    let current_dir = env::current_dir()?;
+    let path = "src/day_two_puzzle_input.txt";
+    let absolute_path = current_dir.join(path);
+    println!("{:?}", absolute_path);
+    let file = File::open(absolute_path)?;
+
+    // Create a BufReader to efficiently read the file line by line
+    let reader = BufReader::new(file);
+    Ok(reader)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ mod day_two;
 
 use day_one::day_one;
 use day_two::day_two_part_one;
+use day_two::day_two_part_two;
 
 fn main() {
     if let Err(err) = day_one() {
         eprintln!("Error: {}", err);
     }
     if let Err(err) = day_two_part_one() {
+        eprintln!("Error: {}", err);
+    }
+    if let Err(err) = day_two_part_two() {
         eprintln!("Error: {}", err);
     }
 }
